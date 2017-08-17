881ca4f567365bedd09b3f9dc18768b86f323293
xiaobaijiadao <657981471@qq.com>
2017-08-17 12:28:25 +0800

    first test fron end

--- www/app.py
+++ www/app.py
@@ -154,7 +154,7 @@ async def init(loop):
 	app = web.Application(loop=loop, middlewares=[
 		logger_factory, response_factory
 	])
-	init_jinja2(app, filter=dict(datetime=datetime_filter))
+	init_jinja2(app, filters=dict(datetime=datetime_filter))
 	add_routes(app, 'handlers')
 	add_static(app)
 	srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
--- www/handlers.py
+++ www/handlers.py
@@ -3,8 +3,10 @@
 
 __author__ = 'WangXiao'
 
-#' url handlers '
+' url handlers '
+
 import re, time, json, logging, hashlib, base64, asyncio
+
 from coroweb import get,post
 from models import User, Comment, Blog, next_id
 
@@ -12,12 +14,22 @@ from models import User, Comment, Blog, next_id
 #编写用于测试的URL处理函数
 @get('/')
 async def index(request):
-	users = await User.findAll() 
-	res = {
-		'__template__' : 'test.html',
-		'users' : users
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+		Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+		Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200),
+	]
+	return {
+		'__template__': 'blogs.html',
+		'blogs': blogs
 	}
-	return res
+	# users = await User.findAll() 
+	# res = {
+	# 	'__template__' : 'test.html',
+	# 	'users' : users
+	# }
+	# return res
     	
 
 @get('/greeting')
