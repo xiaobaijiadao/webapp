e789c323b8ee32a6b27bfcde656fd88c5fe51675
xiaobaijiadao <657981471@qq.com>
2017-08-20 18:03:53 +0800

    list blog

--- www/handlers.py
+++ www/handlers.py
@@ -11,13 +11,27 @@ import markdown2
 
 from aiohttp import web
 
-from apis import APIValueError, APIResourceNotFoundError
+from apis import APIValueError, APIResourceNotFoundError, Page
 from coroweb import get,post
 from models import User, Comment, Blog, next_id
 from config import configs
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
+
+def check_admin(request):
+	if request.__user__ is None or not request.__user__.admin:
+		raise APIPermissionError()
+
+def get_page_index(page_str):
+	p = 1
+	try:
+		p = int(page_str)
+	except ValueError as e:
+		pass
+	if p < 1:
+		p = 1
+	return p
 
 def user2cookie(user, max_age):
 	'''
@@ -28,6 +42,10 @@ def user2cookie(user, max_age):
 	s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
 	L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
 	return '-'.join(L)
+
+def text2html(text):
+	lines = map(lambda s: '<p>%s<p>' % s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt'), filter(lambda s: s.strip() != '', text.split('\n')))
+	return ''.join(lines)
 
 async def cookie2user(cookie_str):
 	'''
@@ -69,12 +87,19 @@ def index(request):
 		'__template__': 'blogs.html',
 		'blogs': blogs
 	}
-	# users = await User.findAll() 
-	# res = {
-	# 	'__template__' : 'test.html',
-	# 	'users' : users
-	# }
-	# return res
+
+@get('/blog/{id}')
+async def get_blog(id):
+	blog = await Blog.find(id)
+	comments  = await Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
+	for c in comments:
+		c.html_content = text2html(c.content)
+	blog.html_content = markdown2.markdown(blog.content)
+	return {
+		'__template__': 'blog.html',
+		'blog': blog,
+		'comments': comments
+	}
     	
 @get('/register')
 def register():
@@ -87,6 +112,14 @@ def signin():
 	return {
 		'__template__': 'signin.html'
 	}
+
+@get('/signout')
+def signout(request):
+	referer = request.headers.get('Referer')
+	r = web.HTTPFound(referer or '/')
+	r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+	logging.info('user signed out.')
+	return r
 
 @post('/api/authenticate')
 async def authenticate(*, email, passwd):
@@ -117,13 +150,20 @@ async def authenticate(*, email, passwd):
 	r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
 	return r
 
-@get('/signout')
-def signout(request):
-	referer = request.headers.get('Referer')
-	r = web.HTTPFound(referer or '/')
-	r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
-	logging.info('user signed out.')
-	return r
+@get('/manage/blogs/create')
+def manage_create_blog():
+	return {
+		'__template__': 'manage_blog_edit.html',
+		'id': '',
+		'action': '/api/blogs'
+	}
+
+@get('/manage/blogs')
+def manage_blogs(*, page='1'):
+	return {
+		'__template__': 'manage_blogs.html',
+		'page_index': get_page_index(page)
+	}
 
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
@@ -152,4 +192,32 @@ async def api_register_user(*, email, name, passwd):
 	r.content_type = 'application/json'
 	r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
 	return r
+
+@get('/api/blogs')
+async def api_blogs(*, page='1'):
+	page_index = get_page_index(page)
+	num = await Blog.findNumber('count(id)')
+	p = Page(num, page_index)
+	if num == 0:
+		return dict(page=p, blogs=())
+	blogs = await Blog.findAll(orderBy='created_at desc', limit=(p.offset, p.limit))
+	return dict(page=p, blogs=blogs)
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*, id):
+	blog = await Blog.find(id)
+	return blog
+
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+	check_admin(request)
+	if not name or not name.strip():
+		raise APIValueError('name', 'name cannot be empty.')
+	if not summary or not summary.strip():
+		raise APIValueError('summary', 'summary cannot be empty.')
+	if not content or not content.strip():
+		raise APIValueError('content', 'content cannot be empty.')
+	blog = Blog(user_id=request._user_.id, user_name=request._user_.name, user_image=request.__user__.image, name=name.strip(), summary=summary.strip(), content=content.strip())
+	await blog.save()
+	return blog
 
